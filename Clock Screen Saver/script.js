0d521d613aebe213fedb7062672270eb602bcd41
not-sample-text <palantonio420@gmail.com>
2023-04-13 21:08:21 +0300

    amogus

---
+++ Clock Screen Saver/script.js
@@ -0,0 +1,17 @@
+function updateTime() {
+	const now = new Date();
+	const year = now.getFullYear();
+	const month = (now.getMonth() + 1).toString().padStart(2, "0");
+	const day = now.getDate().toString().padStart(2, "0");
+	const hours = now.getHours().toString().padStart(2, "0");
+	const minutes = now.getMinutes().toString().padStart(2, "0");
+	const seconds = now.getSeconds().toString().padStart(2, "0");
+
+	const dateElement = document.getElementById("date");
+	dateElement.textContent = `${year}.${month}.${day}`;
+
+	const timeElement = document.getElementById("time");
+	timeElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateTime, 1000); // Call updateTime() every second
